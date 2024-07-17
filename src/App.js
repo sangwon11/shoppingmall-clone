85274f784b5db5b869b24856892848358b843058
sangwon11 <dbstkd0408@gmail.com>
2024-07-17 22:25:35 +0900

    day2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import { Routes, Route} from "react-router-dom";
+import ProductAll from './page/ProductAll';
+import Login from './page/Login';
+import ProductDetail from './page/ProductDetail';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='' element={}/>
-        <Route path='' element={}/>
-        <Route path='' element={}/>
+        <Route path='/' element={<ProductAll/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/product/:id' element={<ProductDetail/>}/>
       </Routes>
     </div>
   );
