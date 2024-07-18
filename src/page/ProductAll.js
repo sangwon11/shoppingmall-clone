793e36e748ed878349be266641a43b6d0944e0e5
sangwon11 <dbstkd0408@gmail.com>
2024-07-18 22:31:38 +0900

    day3

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ProductAll = () => {
+const [productList,setProductList] = useState()
+  const getProducts = async() => {
+    let uri = `http://localhost:4000/products`;
+    let response = await fetch(uri);
+    let data = await response.json();
+    console.log(data);
+  }
+
+useEffect(()=>{
+  getProducts()
+},[])
+
   return (
     <div>ProductAll</div>
   )
