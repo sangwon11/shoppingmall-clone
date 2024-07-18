2a4af919e3dd7f79187cdc160b72af977cf0f058
sangwon11 <dbstkd0408@gmail.com>
2024-07-18 22:05:46 +0900

    day3

--- src/page/component/Navbar.js
+++ src/page/component/Navbar.js
@@ -1,11 +1,32 @@
 import React from 'react'
 
 const Navbar = () => {
+  const menuList = ['여성','devided','남성','신생아/유아',"아동",'h&m Home','Sale','지속가능성']
   return (
     <div>
-        <div></div>
-        <div></div>
-        <div></div>
+
+      <div className='login-button'>
+        <div>로그인</div>
+      </div>
+
+      <div className='nav-section'>
+        <img width={100} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXpRqYm8ZV9DX0FdTQzy6Lpcm6GMKfBbNO7Q&s'/>
+      </div>
+
+      <div className='menu-area'>
+
+          <ul className='menu-list'>
+            {menuList.map((menu) => (
+              <li>{menu}</li>
+            ))}
+          </ul>
+
+        <div>
+          <input type='text'/>
+        </div>
+        
+      </div>
+
     </div>
   )
 }
