cf0699b0566ea9c04c84e66aea05a952bc67302b
sangwon11 <dbstkd0408@gmail.com>
2024-07-20 23:57:27 +0900

    day5

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -5,14 +5,16 @@ import { useSearchParams } from 'react-router-dom';
 
 const ProductAll = () => {
 
-const [productList,setProductList] = useState([])
-const [query, setQuery] = useSearchParams();
-const getProducts = async() =>{
-    let searchQuery = query.get('q') || '';
-    let uri = `http://localhost:4000/products?q=${searchQuery}`;
-    let response = await fetch(uri);
+  const [productList, setProductList] = useState([]);
+  const [query, setQuery] = useSearchParams();
+
+  const getProducts = async() => {
+    let searchQuery = query.get("q") || "";
+    console.log('쿼리값', searchQuery);
+    let url = `http://localhost:4000/products?q=${searchQuery}`;
+    let response = await fetch(url);
     let data = await response.json();
-    setProductList(data)
+    setProductList(data);
 }
 
 
