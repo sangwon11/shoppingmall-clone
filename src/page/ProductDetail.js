eb0ce541e934cc46e53034eb19a222d0acf1ca29
sangwon11 <dbstkd0408@gmail.com>
2024-07-20 22:45:12 +0900

    day5

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
+
+  let { id } = useParams()
+  const [product, setProduct] = useState(null);
+
+  const getProductDetail = async() => {
+    let url = `http://localhost:4000/products/${id}`
+    let reponse = await fetch(url);
+    let data = await reponse.json();
+    console.log("data", data)
+  }
+
+  useEffect(() => {
+    getProductDetail()
+  }, [])
+
   return (
     <div>ProductDetail</div>
   )
