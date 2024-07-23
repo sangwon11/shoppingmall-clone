76106f7555f9577f19d4caafed389d44190b3f91
sangwon11 <dbstkd0408@gmail.com>
2024-07-24 02:03:17 +0900

    배포

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -11,7 +11,7 @@ const ProductAll = () => {
   const getProducts = async () => {
     let searchQuery = query.get("q") || "";
     console.log('쿼리값', searchQuery);
-    let url = `http://localhost:4000/products?q=${searchQuery}`;
+    let url = `https://my-json-server.typicode.com/sangwon11/shoppingmall-clone/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);
