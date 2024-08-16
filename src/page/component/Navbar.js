07c6be1a88a7abd8689ea1f48e09d99616d4e4e5
sangwon11 <dbstkd0408@gmail.com>
2024-08-16 22:53:19 +0900

    nav ui

--- src/page/component/Navbar.js
+++ src/page/component/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import './Navbar.css';
 
 
 const Navbar = () => {
-  const menuList = ['여성','남성','신생아/유아',"아동",'Home','Sale',"봄","여름","가을","겨울"]
+  const menuList = ['SERIES', 'STORE']
   const navigate = useNavigate()
   
   const gotoHome = () => {
@@ -24,28 +25,27 @@ const Navbar = () => {
   return (
     <div className='nav'>
 
-      <div className='login-button' onClick={goToLogin}>
-        <FontAwesomeIcon icon={faUser} style={{margin:3}} />
-        <div>로그인</div>
-      </div>
-
       <div className='nav-section'>
-        <div onClick={gotoHome} className='title'>Flugel</div>
+        <div onClick={gotoHome} className='title'>Sensation</div>
       </div>
 
       <div className='menu-area'>
 
-          <ul className='menu-list'>
+          <ul className='menu-area-list'>
             {menuList.map((menu) => (
               <li>{menu}</li>
             ))}
           </ul>
 
-        <div className='search-bar'>
+        <div className='menu-area-search'>
           <FontAwesomeIcon icon={faMagnifyingGlass} style={{margin:10}} />
           <input type='text' onKeyPress={(event)=>search(event)}/>
         </div>
         
+      </div>
+
+      <div className='login-button' onClick={goToLogin}>
+        <div>Log In</div>
       </div>
 
     </div>
