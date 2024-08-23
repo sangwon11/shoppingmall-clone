87cea8f2fc0cb8245d19c6dac4f73cc8882187de
sangwon11 <dbstkd0408@gmail.com>
2024-08-23 22:30:30 +0900

    상품목록 ui

--- src/page/component/ProductCard.js
+++ src/page/component/ProductCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import './ProductCard.css';
 
 const ProductCard = ({item}) => {
   const navigate = useNavigate();
@@ -7,11 +8,10 @@ const ProductCard = ({item}) => {
     navigate(`/product/${item.id}`);
   }
   return (
-    <div onClick={showDetail}>
+    <div className='product-info' onClick={showDetail}>
         <img className='productList' src={item?.img}/>
-        <div>Consious choice</div>
-        <div>{item?.title}</div>
-        <div>₩{item?.price}</div>
+        <div className='productName'>{item?.title}</div>
+        <div className='productPrice'>₩{item?.price}</div>
         <div>{item?.new == true ? "신제품" : ""}</div>
     </div>
   )
