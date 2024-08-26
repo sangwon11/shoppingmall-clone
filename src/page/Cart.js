e9c424cc63ea0c3a219929073db42d1d721eee70
sangwon11 <dbstkd0408@gmail.com>
2024-08-27 02:16:43 +0900

    1

--- src/page/Cart.js
+++ src/page/Cart.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './Cart.css';
 
 const Cart = () => {
   return (
     <div className='cart-container'>
-      cart
+      <div className='cart'>
+        <div>CART</div>
+        <div>전체삭체</div>
+        <div>비우기</div>
+      </div>
+      <div className='summary'></div>
     </div>
   )
 }
