1c46c773250780ca22265e46ee387e798aa634e8
sangwon11 <dbstkd0408@gmail.com>
2024-08-28 20:27:04 +0900

    1

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Carousel } from 'react-bootstrap';
 import { useNavigate, useParams } from 'react-router-dom';
 import './ProductDetail.css';
 
